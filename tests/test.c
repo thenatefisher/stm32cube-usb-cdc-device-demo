bbd094be75067c79c6294eeda274b83d64cb12ee
thenatefisher <nate.scott.fisher@gmail.com>
2015-08-22 11:17:06 -0700

    add test, some failing

---
+++ tests/test.c
@@ -0,0 +1,41 @@
+#include <errno.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+static void test_1(void **state)
+{
+    assert_false(0);
+}
+
+static void test_2(void **state)
+{
+    assert_false(0);
+}
+
+static void test_3(void **state)
+{
+    assert_false(1);
+}
+
+static void test_4(void **state)
+{
+    assert_false(0);
+}
+
+int main(void)
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_1),
+        cmocka_unit_test(test_2),
+        cmocka_unit_test(test_3),
+        cmocka_unit_test(test_4)
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
